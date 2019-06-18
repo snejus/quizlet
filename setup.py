674dcd427201b0156647516f81103e4ce8e91565
Sarunas Nejus <sarunas@brainlabsdigital.com>
2019-06-18 21:17:30 +0100

    pip3

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
-from pip import req
+from pip3 import req
 
 
 def parse_requirements(filename):
     return [str(ir.req) for ir in req.parse_requirements(filename, session=False)]
 
-__version__ = __import__('quizlet').__version__
+
+__version__ = __import__("quizlet").__version__
 
 setup(
-    name='Quizlet API',
-    version='0.1',
-    author='Aliaksei Sukharevich',
-    author_email='suharevichalexey@gmail.com',
-    description='Python wrapper for Quizlet HTTP API.',
-    packages=['quizlet'],
-    install_requires=parse_requirements('requirements.txt'),
-    tests_require=parse_requirements('requirements-test.txt'),
+    name="Quizlet API",
+    version="0.1",
+    author="Aliaksei Sukharevich",
+    author_email="suharevichalexey@gmail.com",
+    description="Python wrapper for Quizlet HTTP API.",
+    packages=["quizlet"],
+    install_requires=parse_requirements("requirements.txt"),
+    tests_require=parse_requirements("requirements-test.txt"),
     test_suite="tests",
 )
